f0ff7d0c837f47b7d1ed65f8c897a3cdb2c22637
bonjar <bonjarber@gmail.com>
2020-12-02 05:54:14 +0000

    Initial setup for day two part 2

---
+++ two/part_two.py
@@ -0,0 +1,35 @@
+"""
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
+"""
+import re
+from typing import List
+
+
+def solve_p2(passwords: List[str]):
+    total_valid = 0
+    parse_password_regex = "(\d+)-(\d+) ([a-z]): ([a-z]+)"
+    for password_rule in passwords:
+        m = re.match(parse_password_regex, password_rule)
+        min_val = m.group(1)
+        max_val = m.group(2)
+        letter = m.group(3)
+        password = m.group(4)
+
+    return total_valid
+
+
+def main():
+    with open("input.txt") as in_file:
+        return solve_p2(in_file.readlines())
+
+
+if __name__ == "__main__":
+    print(main())
