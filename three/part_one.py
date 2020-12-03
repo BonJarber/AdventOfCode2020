2fcc6b4d683dbb02b36487d151309bc099f72c0c
bonjar <bonjarber@gmail.com>
2020-12-03 06:10:16 +0000

    Working solution for part one

--- three/part_one.py
+++ three/part_one.py
@@ -1,15 +1,25 @@
 """
-
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 """
 from typing import List
 
 
-def solve_p1():
-    pass
+def solve_p1(raw_map: List[str]):
+    total_trees = 0
+    pointer = 0
+    line_size = len(raw_map[0])
+    for i in range(1, len(raw_map)):
+        pointer += 3
+        if pointer >= line_size - 1:
+            pointer -= line_size - 1
+        if raw_map[i][pointer] == "#":
+            total_trees += 1
+
+    return total_trees
 
 
 def main():
-    with open("input.txt") as in_file:
+    with open("test.txt") as in_file:
         return solve_p1(in_file.readlines())
 
 
