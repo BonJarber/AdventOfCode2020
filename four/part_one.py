8235c535000321ac79df5d579d6be8e3cd926101
bonjar <bonjarber@gmail.com>
2020-12-04 04:58:14 +0000

    Initial setup for day four

---
+++ four/part_one.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+from typing import List
+
+
+def solve_p1():
+    pass
+
+
+def main():
+    with open("input.txt") as in_file:
+        return solve_p1(in_file.readlines())
+
+
+if __name__ == "__main__":
+    print(main())
